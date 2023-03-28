f76b1a89005e5258017343cf69235134941e973d
JiabeiZhao <127136536+JiabeiZhao@users.noreply.github.com>
2023-03-27 20:32:08 -0500

    Delete 新建文件夹 directory

--- 新建文件夹/index.js
+++
@@ -1,9 +0,0 @@
-function showPassword() {
-    var passwordField = document.getElementById("password");
-    var checkbox = document.getElementById("show-password");
-    if (checkbox.checked) {
-        passwordField.type = "text";
-    } else {
-        passwordField.type = "password";
-    }
-}
